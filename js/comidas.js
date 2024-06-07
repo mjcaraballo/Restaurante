a54d75c00d1386be1d217d0cda2a25893587afb2
mjcaraballo <mcaraballo.ikaslea.fpe@mendibilformacion.com>
2024-06-07 11:48:30 +0200

    actualizacion

--- js/comidas.js
+++ js/comidas.js
@@ -10,13 +10,32 @@ function leerComidas(lista) {
   let aux = "";
   for (let i = 0; i < lista.length; i++) {
     aux += '<div class="articulos">';
-    aux += `    <a href="${lista[i].enlaceMenu}" title="Menu"><img src="${lista[i].imagen}"></a>`;
+    aux += `    <img onclick="showDetail(${lista[i].id})" src="${lista[i].imagen}">`;
     aux += "    <h2>" + lista[i].titulo + "</h2>";
     aux += `    <p> ${lista[i].descripción}</p>`;
-    aux += "    <a href=" + lista[i].enlaceMenu + ">Ver " + lista[i].titulo + "</a>";
+    aux += `    <a onclick="showDetail(${lista[i].id})">Ver ${ lista[i].titulo}</a>`;
     aux += "</div>";
   }
   comidas.innerHTML = aux;
+}
+
+function showDetail(productId) {
+  
+  const product = lista.find((element) => element.id == productId);
+  console.log(product.enlaceMenu);
+  if (product) {
+    const imagen = document.getElementById("detailImage");
+    imagen.src = product.enlaceMenu;
+    imagen.alt = product.titulo;
+    // document.getElementById('detailTitle').textContent = product.titulo;
+    // document.getElementById('detailDescription').textContent = product.description;
+    // document.getElementById('productDetails').style.display = 'block';
+    document.querySelector(".modal").style.display = "block";
+  }
+}
+
+function closeDetails() {
+  document.getElementById("productDetails").style.display = "none";
 }
 
 
